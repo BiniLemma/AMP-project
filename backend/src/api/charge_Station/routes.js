9fea152df7d17e497d131f7c9a07cf7f9701edde
12yeabsra <yeabsraaemro12@gmail.com>
2023-05-29 22:27:02 +0300

    Added routes and controller

---
+++ backend/src/api/charge_Station/routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const chargeStationController = require("./contoller");
+const router = express.Router();
+
+const { protect, authorize } = require('../../middleware/auth.js')
+
+router.route("/")
+    .post(protect, authorize("provider"), chargeStationController.createChargeStation)
+    .get(protect, chargeStationController.getMyChargeStations)
+
+router.route("/comment")
+    .post(protect, chargeStationController.commentChargeStation)
+    .delete(protect, chargeStationController.deleteComment)
+    .patch(protect, chargeStationController.updateComment)
+
+router.route("/:id")
+    .get(protect, chargeStationController.getChargeStation)
+    .delete(protect, chargeStationController.deleteChargeStation)
+    .patch(protect, chargeStationController.updateChargeStation)
+
+router.route("/all")
+    .get(protect, chargeStationController.getAllChargeStation)
+
+router.route("/rate")
+    .post(protect, chargeStationController.rateChargeStation)
+
+router.route("/search")
+    .post(chargeStationController.getNearChargeStations)
+
+module.exports = router;
